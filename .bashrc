f2e8ec933924a3c2239177553c5bc6061a40208c
Ramon Fried <ramon.fried@gmail.com>
2015-04-09 21:40:37 +0300

    Added acd_func and local bashrc

--- .bashrc
+++ .bashrc
@@ -9,7 +9,8 @@ case $- in
 esac
 
 source ~/.git-prompt
-
+source ~/.localbash.rc
+source ~/.acd_func
 # append to the history file, don't overwrite it
 shopt -s histappend
 
--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=$PWD                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files=".bashrc .gitignore .gitconfig .vimrc .vim .git-prompt"    # list of files/folders to symlink in homedir
+files=".bashrc .gitignore .gitconfig .vimrc .vim .git-prompt .acd_func"    # list of files/folders to symlink in homedir
 
 ##########
 
